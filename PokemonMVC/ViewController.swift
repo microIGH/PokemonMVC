7ea84e6098b25345724c947f9f3f19ef2a5e3572
ISRAEL GARCIA <micro4004@yahoo.com.mx>
2025-08-07 13:57:40 -0600

    Initial Commit

---
+++ PokemonMVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonMVC
+//
+//  Created by ISRAEL GARCIA on 07/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
